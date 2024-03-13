1f09b1c05c144bc6dcd3bcafc57a54752dadbad4
gloriaadanna <gloriaagwam7@gmail.com>
2024-03-13 20:23:50 +0000

    task

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-"""Lockboxes Contains method that finds the keys to
-open other lockboxes
-"""
+"""this is the Solution to Lockboxes problem"""
 
 
 def canUnlockAll(boxes):
-    unlocked = set()
-
-    for box_id, box in enumerate(boxes):
-        if len(box) == 0 or box_id == 0:
-            unlocked.add(box_id)
-        for key in box:
-            if key < len(boxes) and key != box_id:
-                unlocked.add(key)
-        if len(unlocked) == len(boxes):
-            return True
-    return False
+    """This Determines if all the boxes can be opened"""
+    if (type(boxes) is not list or len(boxes) == 0):
+        return False
+    for k in range(1, len(boxes) - 1):
+        unlocked = False
+        for i in range(len(boxes)):
+            unlocked = k in boxes[i] and k != i
+            if unlocked:
+                break
+        if unlocked is False:
+            return unlocked
+    return True

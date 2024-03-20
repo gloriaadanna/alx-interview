118d8a0f79a4dc8eb5d2c139c8d17ff376f68ce2
gloriaadanna <gloriaagwam7@gmail.com>
2024-03-20 07:56:24 +0000

    task

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
 """
-Minimum Operations
-Given num n, write a method that calculates the fewest number of operations
-needed to result in exactly n H characters in a file
-Prototype: def minOperations(n)
-Returns an integer
-if n is impossible to achieve, return 0
-"""
+In a text file, there is a single character H. Your text editor
+can execute only two operations in this file: `Copy All` and `Paste`.
+
+@TODO:
+        Deines a method that calculates the fewest number of operations
+            needed to result in exactly n H characters in the file.
+            """
 
 
-def minOperations(n):
-        """
-            Function minOperations
-                Returns an integer
-                    """
-                        result = 0
-                            x = 2
-                                while n > 1:
-                                        while n % x == 0:
-                                                    result += x
-                                                                n /= x
-                                                                        x += 1
-                                                                            return result
+            def minOperations(n):
+                """
+                calculates the fewest number of operations needed to result n H characters in the files
+                Args:
+                n (int): length of letter 'H' required in the filed
+                Returns:
+                (int): number of minimum operations if possible
+                """
+                minimumOperations = 2
+                totalOperations = 0
+                while n > 1:
+                    while n % minimumOperations == 0:
+                         totalOperations += minimumOperations
+                         n /= minimumOperations
+                          minimumOperations += 1
+                          return totaloperations
